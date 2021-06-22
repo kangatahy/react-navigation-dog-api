5402958c59bc60fee5a65b9b702a3d8192edb566
Khelvin Angatahy <khellsjunior13@gmail.com>
2021-06-22 15:56:27 +0200

    Navigation basique entre les pages

--- App.js
+++ App.js
@@ -1,14 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 
-function HomeScreen() {
+function HomeScreen({navigation}) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Voici l'ecran d'accueil</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button 
+      onPress={()=> navigation.push('Details')}
+      title={'Et là on continue d\'aller sur Details'}/>
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button title={'Go Back to first screen in stack'} onPress={()=> {navigation.popToTop()}}/>
     </View>
   );
 }
@@ -18,8 +35,9 @@ const Stack = createStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
